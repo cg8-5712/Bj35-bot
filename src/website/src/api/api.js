11c4d02882f3961ad79946b6bd2491c42d0489ad
cg8 <5712.cg8@gmail.com>
2025-03-02 23:40:38 +0800

    refactor(api): conbine multiple params to one param

--- src/website/src/api/api.js
+++ src/website/src/api/api.js
@@ -1,5 +1,5 @@
 // api.js
-export async function robotId() {
+export async function status() {
     const id = '1309097125891674112';
     const headers = new Headers();
     headers.append('Accept', 'application/json');
@@ -16,7 +16,7 @@ export async function robotId() {
 
         const res_data = await res.json();
 
-        return res_data['data']['deviceInfo']['deviceId'];
+        return res_data;
 
     } catch (error) {
         console.error(error);
@@ -24,27 +24,27 @@ export async function robotId() {
     }
 }
 
-export async function powerPercent() {
-    const id = '1309097125891674112';
-    const headers = new Headers();
-    headers.append('Accept', 'application/json');
-    headers.append('Content-Type', 'application/json');
-    headers.append('token', 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiIsImFkbWluIjp0cnVlfQ.')
-    try {
-        const res = await fetch(`http://127.0.0.1:8080/api/v1/device_status/${id}`, {
-            method: 'GET',
-            headers: headers
-        });
-        if (!res.ok) {
-            throw new Error('NetworkError');
-        }
-
-        const res_data = await res.json();
-
-        return res_data['data']['deviceStatus']['powerPercent'];
-
-    } catch (error) {
-        console.error(error);
-        throw error;
-    }
-}
+// export async function powerPercent() {
+//     const id = '1309097125891674112';
+//     const headers = new Headers();
+//     headers.append('Accept', 'application/json');
+//     headers.append('Content-Type', 'application/json');
+//     headers.append('token', 'eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiSm9obiIsImFkbWluIjp0cnVlfQ.')
+//     try {
+//         const res = await fetch(`http://127.0.0.1:8080/api/v1/device_status/${id}`, {
+//             method: 'GET',
+//             headers: headers
+//         });
+//         if (!res.ok) {
+//             throw new Error('NetworkError');
+//         }
+//
+//         const res_data = await res.json();
+//
+//         return res_data['data']['deviceStatus']['powerPercent'];
+//
+//     } catch (error) {
+//         console.error(error);
+//         throw error;
+//     }
+// }
