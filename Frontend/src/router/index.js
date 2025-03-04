f5689608329dcc1e7f40114bd9e27c236cdce2a1
AptS:1547 <esaps@esaps.net>
2025-03-04 17:05:53 +0800

    feat: add message info component and dashboard view
    - Add MessageInfo.vue component for displaying notifications
    - Update router to include new dashboard view
    - Modify status.vue to integrate MessageInfo component
    - Adjust HTML structure for full-height layout
    
    These changes improve the user interface by adding a notification system and a new dashboard view, enhancing the overall user experience.

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import status from '@/views/status.vue'
+import status from '@/views/Status.vue'
+import dashboard from '@/views/Dashboard.vue'
 import NotFound from '@/views/NotFound.vue'
 
 const routes = [
   { path: '/', name: 'status', component: status },
+  { path: '/dashboard', name: 'dashboard', component: dashboard},
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound }
 ]
 
